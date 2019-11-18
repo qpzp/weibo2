9479ff43508a87cbab348199da1626531f9b0c49
qpzp <qpzp@hotmail.com>
2019-11-18 13:05:24 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <title>@yield('title','微博') - 电动少女</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','主✌')
+@section('content')
+    <h1>🐖✌</h1>
+@stop
