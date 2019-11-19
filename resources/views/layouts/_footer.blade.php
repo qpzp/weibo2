672f454ff81f4aa6e0d166c78edf880408b208d2
qpzp <qpzp@hotmail.com>
2019-11-19 15:19:17 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer">
     <a href="#">不要走</a>
-    <div class="float-right"><a href="/about">关于</a></div>
+    <div class="float-right"><a href="{{route('about')}}">关于</a></div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
     <div class="container">
-        <a class="navbar-brand" href="/">electric</a>
+        <a class="navbar-brand" href="{{route('home')}}">electric</a>
         <ul class="navbar-nav justify-content-end">
             <li class="nav-item">
-                <a class="nav-link" href="/help">帮助</a>
+                <a class="nav-link" href="{{route('help')}}">帮助</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">登录</a>
