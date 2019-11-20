43ae11d95dc317ef43c0d32800dabb73450368ff
qpzp <qpzp@hotmail.com>
2019-11-20 13:08:14 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    //创建用户的页面
     public function create()
     {
         return view('users.create');
+    }
+
+    //显示用户个人信息的页面
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
