b302a06a3763c57da49ceff8c01337023dae64ca
qpzp <qpzp@hotmail.com>
2019-11-23 13:33:42 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'xuqi';
         $user->email = 'xuqi@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,4 +1,11 @@
 <div class="list-group-item">
     <img src="{{$user->gravatar()}}" alt="{{$user->name}}" class="mr-3 gravatar">
     <a href="{{route('users.show',$user)}}">{{$user->name}}</a>
+    @can('destroy',$user)
+        <form action="{{route('users.destroy',$user->id)}}" method="post" class="float-right">
+            {{csrf_field()}}
+            {{method_field('DELETE')}}
+            <button type="submit" class="btn btn-danger btn-sm delete-btn">delete</button>
+        </form>
+    @endcan
 </div>
