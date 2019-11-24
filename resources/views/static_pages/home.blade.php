98b42233c1b9c0963cf47c95792cbb3f268b6f3a
qpzp <qpzp@hotmail.com>
2019-11-24 08:43:46 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.default')
 @section('title','主✌')
 @section('content')
-    <div class="jumbotron">
-        <h1 class="display-4">她不停的旋转</h1>
-        <p class="lead">在空荡的舞台</p>
-        <hr class="my-4">
-        <a class="btn btn-primary btn-lg" href="{{route('signup')}}" role="button">注册</a>
+  @if(Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info',['user'=>Auth::user()])
+        </section>
+      </aside>
     </div>
+  @else
+    <div class="jumbotron">
+      <h1 class="display-4">她不停的旋转</h1>
+      <p class="lead">在空荡的舞台</p>
+      <hr class="my-4">
+      <a class="btn btn-primary btn-lg" href="{{route('signup')}}" role="button">注册</a>
+    </div>
+  @endif
 @stop
