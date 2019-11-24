b56bd498620d3be5b971b0197ccb6d9ecb93b0c8
qpzp <qpzp@hotmail.com>
2019-11-24 06:02:10 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,48 +9,53 @@ use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    protected $table = 'users';
+  protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function gravatar($size = 200)
-    {
-        $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
-    }
+  public function gravatar($size = 200)
+  {
+    $hash = md5(strtolower(trim($this->attributes['email'])));
+    return "http://www.gravatar.com/avatar/$hash?s=$size";
+  }
 
-    public static function boot()
-    {
-        parent::boot();
-        static::creating(function ($user) {
-            $user->activation_token = Str::random(10);
-        });
-    }
+  public static function boot()
+  {
+    parent::boot();
+    static::creating(function ($user) {
+      $user->activation_token = Str::random(10);
+    });
+  }
+
+  public function statuses()
+  {
+    return $this->hasMany(Status::class);
+  }
 }
